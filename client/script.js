10fdac2e6fb37cc1ad8a141d8cacfa13dbe80048
x_yue <103266340+CrpeDiem@users.noreply.github.com>
2023-04-14 10:01:45 +0200

    first commit

---
+++ client/script.js
@@ -0,0 +1,67 @@
+const form = document.getElementById('form');
+const chat_container = document.getElementById('chat_container');
+
+function chat_rows(ai, value, id) {
+    return (
+        `
+        <div class="chat-row ${ai? "ai" : "user"}">
+            <div class="chat-row-content">
+                <div class="role">
+                    <span>${ai ? "AI" : "user"}</span>
+                </div>
+                <div class="message" id=${id}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+function generateId() {
+    const timeStamp = Date.now();
+    const randomNum = Math.random();
+    const endNum = randomNum.toString(8);
+
+    return `${timeStamp}-${endNum}`;
+}
+
+const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    chat_container.innerHTML += chat_rows(false, data.get('inputMessage'));
+    form.reset();
+
+    const responseId = generateId();
+    // chat_container.innerHTML += chat_rows(true, " ", responseId);
+
+    chat_container.scrollTop = chat_container.scrollHeight;
+
+    // const responseMessage = document.getElementById(responseId);
+
+
+    const response = await fetch(' http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: data.get('inputMessage')
+        })
+    })
+
+    if (!response.ok) {
+        throw new Error("Something wrong!");
+    } else {
+        const data = await response.json();
+        chat_container.innerHTML += chat_rows(true, data.bot, responseId);
+    }
+}   
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        handleSubmit(e);
+    }
+})
---
+++ server/server.js
@@ -0,0 +1,66 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.get("/", async (req, res) => {
+    // const { message } = req.body;
+
+//     const response = await openai.createChatCompletion({
+//         model: "gpt-3.5-turbo",
+//         messages: [
+//             {
+//                 role: "user",
+//                 content: `${message}`
+//                 // content: "hi there"
+//             }
+//         ]
+//     });
+//     // res.json({
+//     //     bot: response.data.choices[0].message.content
+//     // })
+
+//     res.status(200).send({
+//         message: response.data.choices[0].message.content
+//     })
+// })
+
+app.post("/", async (req, res) => {
+    try {
+        const { message } = req.body;
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: `${message}`
+                    // content: "hi there"
+                }
+            ]
+        });
+        // console.log(response.data.choices[0].message);
+        res.status(200).send({
+            bot: response.data.choices[0].message.content
+        })
+    } catch (error) {
+        res.status(500).send({
+            error: error.message
+        })
+    }
+})
+
+app.listen(5000, () => {
+    console.log("Example app listening at http://localhost:5000")
+})
